eb2f5fb74d5cbb87b2697c1dd04787656a31ee92
SimoneSaramondi <simo98sara@gmail.com>
2021-04-07 15:34:20 +0200

    aggiunta di ulteriori modi di svolgimento

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
+
 var nome = prompt("Inserisci il tuo nome");
 
 var cognome = prompt("Inserisci il tuo cognome");
@@ -5,3 +6,17 @@ var cognome = prompt("Inserisci il tuo cognome");
 var colore = prompt("Inserisci il tuo colore prefito");
 
 document.writeln(nome + cognome + colore + "21");
+
+/*ALTRI MODI PER SVOLGERE L'ESERCIZIO*/
+/* SECONDO MODO
+document.writeln(nome);
+
+document.writeln(cognome);
+
+document.writeln(colore);
+
+document.writeln("21");
+*/
+/* TERZO MODO
+document.getElementById("info-personali").innerHTML= nome + cognome + colore +"21";
+*/
